d29c1e191fdc585b4460b133f04572b8ea11e622
mateuszfranke <mateuszfranke@icloud.com>
2021-02-27 12:30:54 +0100

    Open/close window

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+//Selectors
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonCloseModal = document.querySelector('.close-modal');
+const buttonsOpenModal = document.querySelectorAll('.show-modal');
+//
+
+//eventListeners
+for(let i=0;i<buttonsOpenModal.length;i++){
+   buttonsOpenModal[i].addEventListener('click', showModal);
+} 
+buttonCloseModal.addEventListener('click',closeModal);
+//
+
+function closeModal(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+function showModal(){
+   modal.classList.remove('hidden');
+   overlay.classList.remove('hidden');
+}
+
