262ab980c8ff83c49bdc72e8c4843866d1ea1828
mateuszfranke <mateuszfranke@icloud.com>
2021-02-27 12:49:53 +0100

    Readme.md added

---
+++ README.md
@@ -0,0 +1,9 @@
+# modal-windows
+
+Popping up modal window while blurring the rest of the screen. Escape key also closes the modal window. [Available here](https://mateuszfranke.github.io/modal-windows/)
+
+## Stack
+
+- JavaScript
+- CSS3
+- HTML5
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const escapeKey = 'Escape';
 //Selectors
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
@@ -8,19 +9,23 @@ const buttonsOpenModal = document.querySelectorAll('.show-modal');
 //
 
 //eventListeners
-for(let i=0;i<buttonsOpenModal.length;i++){
-   buttonsOpenModal[i].addEventListener('click', showModal);
-} 
-buttonCloseModal.addEventListener('click',closeModal);
+for (let i = 0; i < buttonsOpenModal.length; i++) {
+    buttonsOpenModal[i].addEventListener('click', showModal);
+}
+buttonCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (event) {
+    if (event.key === escapeKey) closeModal();
+})
 //
 
-function closeModal(){
+function closeModal() {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
 
-function showModal(){
-   modal.classList.remove('hidden');
-   overlay.classList.remove('hidden');
+function showModal() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 }
 
